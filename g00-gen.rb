ddfc330a0d4434704a7b6c0c7acd0a8d92c42c02
applehq <theappleman@gmail.com>
2008-11-22 16:27:23 +0000

    add g00 generator script
    this is a cross-platform ruby script for generating the edited image
    files for Little Busters!. Hopefully soon it won't need to be used much
    soon...

---
+++ g00-gen.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# Generate modified/translated G00 files from lbimages
+# Needs 3 variables: base, vaconv and final.
+# base is the root directory of the svn checkout (no trailing slash)
+# vaconv is the full path to the vaconv binary
+# final is the directory that you want the G00s to end up in (no trailing slash)
+# if you are running windows, ensure any \'s are slash-escaped
+
+# Begin configuration
+
+base = ""
+vaconv = ""
+final = ""
+
+# End configuration
+
+exit if base == "" or final == "" or vaconv !~ /vaconv(\.exe)?$/
+require 'ftools'
+temp = final + File::SEPARATOR + "temp"
+Dir.mkdir(final) unless File.exists?(final) and File.directory?(final)
+Dir.mkdir(temp) unless File.exists?(temp) and File.directory?(temp)
+vaconvopts = "--g00=2"
+
+dir = Dir.entries(base) - %w[. .. .svn .git .svn-index Templates Trial\ Edition]
+dir.each { |e|
+  full = base + File::SEPARATOR + e
+  if File.directory?(full)
+    sub = Dir.entries(full) - %w[. .. .svn .git .svn-index]
+    sub.each { |l|
+      if l =~ /\.png$/
+	bm = full + File::SEPARATOR + l
+	File.copy(bm, temp)
+	bn = full + File::SEPARATOR + "xml" + File::SEPARATOR + l[/^(.*?)\.png/, 1] + ".xml"
+	File.copy(bn, temp) if File.exists?(bn) and File.file?(bn) and File.readable?(bn)
+
+	img = temp + File::SEPARATOR + l
+	system vaconv, vaconvopts, "-d", final, img
+      end
+    }
+  end
+}
